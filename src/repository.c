13d04722322f25c66a99a44468f2f4990d295c60
Grant <241880414@smail.nju.edu.cn>
2024-12-21 15:36:42 +0800

    change the foder name from gitm to .gitm

--- src/repository.c
+++ src/repository.c
@@ -3,33 +3,33 @@
 
 //init
 int init(){
-    char const *path="./gitm";
+    char const *path="./.gitm";
     //创建.gitm文件夹，创建失败即说明已经有仓库了，返回-1
     if(make_directory(path)){
         return -1;
     }
 
     //创建（对象）objects文件
-    if(make_directory("./gitm/objects")){
+    if(make_directory("./.gitm/objects")){
         return -1;
     };
 
     //创建（日志）logs文件
-    if(make_directory("./gitm/logs")){
+    if(make_directory("./.gitm/logs")){
         return -1;
     };
 
     //创建refs文件，存放各种引用，如分支、标签等的指针文件
-    if(make_directory("./gitm/refs")){
+    if(make_directory("./.gitm/refs")){
         return -1;
     };
         //在其下创建head文件，存放指向分支指针
-        if(make_directory("./gitm/refs/head")){
+        if(make_directory("./.gitm/refs/head")){
             return -1;
         };
 
         //创建tags文件，存放指向分支指针（哈希值？）
-        if(make_directory("./gitm/refs/tags")){
+        if(make_directory("./.gitm/refs/tags")){
             return -1;
         };\
     return 0;
